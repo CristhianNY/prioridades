3f1abc3fd374bca54eb28bd89a0da605d318c9c9
Cristhian Bonilla <cristhian4545@gmail.com>
2020-06-19 08:06:21 -0500

    rename project

---
+++ app/src/main/java/com/cristhianbonilla/prioridades/App.kt
@@ -0,0 +1,14 @@
+package com.cristhianbonilla.prioridades
+
+import android.app.Application
+
+class App : Application() {
+
+    override  fun onCreate(){
+        super.onCreate()
+    }
+
+    companion object {
+        val contractAddress: String = ""
+    }
+}
---
+++ data/src/main/java/com/cristhianbonilla/data/repository/login/LoginRepository.kt
@@ -0,0 +1,4 @@
+package com.cristhianbonilla.data.repository.login
+
+class LoginRepository {
+}
