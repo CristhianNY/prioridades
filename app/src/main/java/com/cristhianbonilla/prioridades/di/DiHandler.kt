a748e5cd3da4c79906fd7a3c2c152d9ef5d12681
Cristhian Bonilla <cristhian4545@gmail.com>
2020-06-24 11:21:27 -0500

    add new home module

---
+++ app/src/main/java/com/cristhianbonilla/prioridades/di/DiHandler.kt
@@ -0,0 +1,8 @@
+package com.cristhianbonilla.prioridades.di
+
+import org.koin.core.module.Module
+
+internal fun getModules() = mutableListOf<Module>().apply {
+
+
+}
---
+++ app/src/main/java/com/cristhianbonilla/prioridades/di/data/LoginDataModule.kt
@@ -0,0 +1,7 @@
+package com.cristhianbonilla.prioridades.di.data
+
+import org.koin.dsl.module
+
+internal val loginDataModule = module {
+
+}
