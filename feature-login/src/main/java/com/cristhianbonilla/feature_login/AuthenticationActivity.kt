fddf03348fa5731829d6c4eafabae2f3ce028ca2
Cristhian Bonilla <cristhian4545@gmail.com>
2020-07-16 22:29:14 -0500

    Crear fragmentos principales para login

--- feature-login/src/main/java/com/cristhianbonilla/feature_login/AuthenticationActivity.kt
+++ feature-login/src/main/java/com/cristhianbonilla/feature_login/AuthenticationActivity.kt
@@ -3,7 +3,7 @@ package com.cristhianbonilla.feature_login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LoginActivity : AppCompatActivity() {
+class   AuthenticationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
