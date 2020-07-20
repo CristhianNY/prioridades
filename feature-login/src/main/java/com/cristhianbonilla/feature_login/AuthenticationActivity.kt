77d36dda8d39530f117d8c994ea1b9d8701cfa4e
Cristhian Bonilla <cristhian4545@gmail.com>
2020-07-20 16:15:35 -0500

    Agregar animaciones y redireccionamiento login

--- feature-login/src/main/java/com/cristhianbonilla/feature_login/AuthenticationActivity.kt
+++ feature-login/src/main/java/com/cristhianbonilla/feature_login/AuthenticationActivity.kt
@@ -2,10 +2,14 @@ package com.cristhianbonilla.feature_login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cristhianbonilla.feature_login.databinding.ActivityLoginBinding
 
 class   AuthenticationActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
     }
 }
