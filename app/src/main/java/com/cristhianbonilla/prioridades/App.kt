bfbbcbc8b14e8d7add16d1babd3d6f33e6719b1f
Cristhian Bonilla <cristhian4545@gmail.com>
2020-07-22 08:28:24 -0500

    configurar aplicaiton

--- app/src/main/java/com/cristhianbonilla/prioridades/App.kt
+++ app/src/main/java/com/cristhianbonilla/prioridades/App.kt
@@ -1,11 +1,24 @@
 package com.cristhianbonilla.prioridades
 
 import android.app.Application
+import com.cristhianbonilla.domain.config.ModeTypeModel
+import com.cristhianbonilla.prioridades.BuildConfig.*
+import com.cristhianbonilla.prioridades.config.AppConfigurationModel
+import com.cristhianbonilla.prioridades.di.getModules
 
 class App : Application() {
 
-    override  fun onCreate(){
+    override fun onCreate() {
         super.onCreate()
+        initCore(
+            AppConfigurationModel(
+                ModeTypeModel.DEBUG,
+                BASE_URL,
+                VERSION_CODE,
+                VERSION_NAME,
+                getModules()
+            )
+        )
     }
 
     companion object {
