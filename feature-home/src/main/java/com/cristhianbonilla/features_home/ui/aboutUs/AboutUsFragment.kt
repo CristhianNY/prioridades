f5ef3415f46d5d060aa0b339df45b7482d343df2
Cristhian Bonilla <cristhian4545@gmail.com>
2020-07-29 12:23:55 -0500

    cambiar nombre fragmento por defecto por about us

--- feature-home/src/main/java/com/cristhianbonilla/features_home/ui/aboutUs/AboutUsFragment.kt
+++ feature-home/src/main/java/com/cristhianbonilla/features_home/ui/aboutUs/AboutUsFragment.kt
@@ -1,4 +1,4 @@
-package com.cristhianbonilla.features_home.ui.notifications
+package com.cristhianbonilla.features_home.ui.aboutUs
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,9 +10,9 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.cristhianbonilla.feature_home.R
 
-class NotificationsFragment : Fragment() {
+class AboutUsFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
+    private lateinit var notificationsViewModel: AboutUsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,7 +20,7 @@ class NotificationsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         notificationsViewModel =
-            ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
+            ViewModelProviders.of(this).get(AboutUsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_notifications, container, false)
         val textView: TextView = root.findViewById(R.id.text_notifications)
         notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
