ca216a7cd89c929e0cef81c5359b8c574b6d18dd
Cristhian Bonilla <cristhian4545@gmail.com>
2020-08-02 16:39:33 -0500

    renombrar fragmento

--- feature-home/src/main/java/com/cristhianbonilla/features_home/ui/aboutUs/AboutUsFragment.kt
+++ feature-home/src/main/java/com/cristhianbonilla/features_home/ui/aboutUs/AboutUsFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.cristhianbonilla.feature_home.R
 
@@ -21,7 +19,7 @@ class AboutUsFragment : Fragment() {
     ): View? {
         notificationsViewModel =
             ViewModelProviders.of(this).get(AboutUsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
+        val root = inflater.inflate(R.layout.fragment_about_us, container, false)
         return root
     }
 }
