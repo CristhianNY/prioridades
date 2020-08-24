80f551e3e121bfb5f082278d066a5f132b3d2262
Cristhian Bonilla <cristhian4545@gmail.com>
2020-08-24 17:59:33 -0500

    lectura del pdf

--- feature-magazine-reader/src/main/java/com/cristhianbonilla/feature_magazine_reader/magazineViewer/ReaderMagazineActivity.kt
+++ feature-magazine-reader/src/main/java/com/cristhianbonilla/feature_magazine_reader/magazineViewer/ReaderMagazineActivity.kt
@@ -3,7 +3,9 @@ package com.cristhianbonilla.feature_magazine_reader.magazineViewer
 import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
+import android.util.Base64
 import android.view.View
+import android.view.WindowManager
 import android.webkit.WebResourceError
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
@@ -12,69 +14,60 @@ import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.cristhianbonilla.feature_magazine_reader.R
+import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener
+import com.github.barteksc.pdfviewer.util.FitPolicy
 import kotlinx.android.synthetic.main.activity_reader_magazine.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.io.InputStream
+import java.net.URL
 
 
-class ReaderMagazineActivity : AppCompatActivity() {
+class ReaderMagazineActivity : AppCompatActivity(), OnLoadCompleteListener {
     @RequiresApi(Build.VERSION_CODES.HONEYCOMB)
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_reader_magazine)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_SECURE,
+            WindowManager.LayoutParams.FLAG_SECURE
+        )
+
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val magazineUrl = intent.getStringExtra("URL_MAGAZINE")
-        showPdfFile(magazineUrl.toString())
+        //  showPdfFile(magazineUrl.toString())
+        magazineUrl?.let { showPdfBase64(it) }
     }
 
-    @RequiresApi(Build.VERSION_CODES.HONEYCOMB)
-    @SuppressLint("SetJavaScriptEnabled")
-    private fun showPdfFile(magazineUrl: String) {
-    val filename =
-            "https://docs.google.com/gview?embedded=true&url=$magazineUrl"
-     //   val filename ="https://www.artistasamerica.com/"
+    @RequiresApi(Build.VERSION_CODES.FROYO)
+    fun showPdfBase64(magazineUrl: String) {
+        GlobalScope.launch {
+            val input: InputStream = URL(magazineUrl).openStream()
+            webView.fromBytes(input.readBytes())
+                .enableSwipe(true) // allows to block changing pages using swipe
+                .swipeHorizontal(false)
+                .enableDoubletap(true)
+                .defaultPage(0)
+                .enableAnnotationRendering(false) // render annotations (such as comments, colors or forms)
+                .password(null)
+                .scrollHandle(null)
+                .pageSnap(true)
+                .enableAntialiasing(true) // improve rendering a little bit on low-res screens
+                // spacing between pages in dp. To define spacing color, set view background
+                .spacing(0)
+                .pageFitPolicy(FitPolicy.WIDTH)
+                .load()
 
-        val idsToHide =
-            arrayOf("ndfHFb-c4YZDc-Wrql6b")
-        webView.settings.javaScriptEnabled = true
-        webView.settings.javaScriptEnabled = true
-        webView.settings.loadWithOverviewMode = true
-        webView.settings.useWideViewPort = true
-        webView.settings.builtInZoomControls = true
-        webView.settings.displayZoomControls = false
-
-        webView.webViewClient = object : WebViewClient() {
-            override fun onPageFinished(view: WebView, url: String) {
-                // TODO Auto-generated method stub
-                super.onPageFinished(view, url)
-
-                if (view.title.equals("")){
-                    view.reload()
-                }else{
-                    webView.settings.javaScriptEnabled = true
-
-                    with(view.loadUrl("javascript:(function() { " +
-                            "document.getElementsByClassName('ndfHFb-c4YZDc-Wrql6b')[0].style.display='none'; })()")){
-                        webView.visibility = View.VISIBLE
-
-                    }
-
-                    pb.visibility = View.GONE
-
-                }
-            }
-
-            override fun onReceivedError(
-                view: WebView?,
-                request: WebResourceRequest?,
-                error: WebResourceError?
-            ) {
-                Toast.makeText(applicationContext, error.toString(), Toast.LENGTH_SHORT).show();
-            }
         }
-        webView.loadUrl(filename)
+    }
 
+    override fun loadComplete(nbPages: Int) {
+        Toast.makeText(this, "cargo", Toast.LENGTH_LONG).show()
+        // pb.visibility = View.GONE
     }
 
 }
