a997a0b40a9b5e148281dd0de87088be2fe9d2bb
Cristhian Bonilla <cristhian4545@gmail.com>
2020-08-26 15:12:53 -0500

    agregar validación

--- feature-login/src/main/java/com/cristhianbonilla/feature_login/register/RegisterFragment.kt
+++ feature-login/src/main/java/com/cristhianbonilla/feature_login/register/RegisterFragment.kt
@@ -1,13 +1,50 @@
 package com.cristhianbonilla.feature_login.register
 
+import android.os.Bundle
+import android.view.View
+import com.afollestad.vvalidator.form
 import com.cristhianbonilla.feature_login.BR
 import com.cristhianbonilla.feature_login.R
 import com.cristhianbonilla.feature_login.databinding.FragmentRegisterBindingImpl
 import com.cristhianbonilla.foundations.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_register.*
 
 class RegisterFragment : BaseFragment<
         RegisterUserState,
         RegisterData,
         RegisterTracker,
         RegisterViewModel,
-        FragmentRegisterBindingImpl>(R.layout.fragment_register, BR.viewModel, BR.data)
+        FragmentRegisterBindingImpl>(R.layout.fragment_register, BR.viewModel, BR.data){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        form {
+            input(R.id.edit_name) {
+                isNotEmpty().description(R.string.username_required)
+            }
+            input(R.id.editLastName) {
+                isNotEmpty().description(R.string.last_name_required)
+            }
+            input(R.id.editEmail ) {
+                isNotEmpty().description(R.string.email_required)
+            }
+            input(R.id.editPassword) {
+                isNotEmpty().description(R.string.password_required)
+            }
+            input(R.id.editPassword){
+               isNotEmpty()
+            }
+
+            input(R.id.editConfirmPassword){
+                conditional({ R.id.editPassword != R.id.editConfirmPassword }){
+                    assert("La contraseña no coincide ") { view -> false }
+
+                }
+            }
+            submitWith(R.id.btnNextRegister) { result ->
+            viewModel.goToRegisterStep2()
+            }
+        }
+
+    }
+}
