529d0bb27944d8b8678256baa457ec55edb2a884
admariachi <admariachi@gmail.com>
2020-08-30 11:14:23 -0500

    ajustar ventana about us

--- feature-home/src/main/java/com/cristhianbonilla/features_home/ui/aboutUs/AboutUsFragment.kt
+++ feature-home/src/main/java/com/cristhianbonilla/features_home/ui/aboutUs/AboutUsFragment.kt
@@ -1,12 +1,16 @@
 package com.cristhianbonilla.features_home.ui.aboutUs
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.cristhianbonilla.feature_home.R
+
 
 class AboutUsFragment : Fragment() {
 
@@ -20,6 +24,17 @@ class AboutUsFragment : Fragment() {
         notificationsViewModel =
             ViewModelProviders.of(this).get(AboutUsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_about_us, container, false)
+
+        val btnEmail = root.findViewById<Button>(R.id.btn_send_email)
+        btnEmail.setOnClickListener {
+            val emailIntent = Intent(
+                Intent.ACTION_SENDTO, Uri.fromParts(
+                    "mailto", "soporte@revistaprioridades.com", null
+                )
+            )
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Soporte App")
+            requireContext().startActivity(Intent.createChooser(emailIntent, null))
+        }
         return root
     }
 }
